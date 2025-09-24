373a533494e60801e7c7bd8e383ca57ef3554046
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-09-24 01:57:46 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/taks/taks_model.dart
@@ -0,0 +1,12 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import '/index.dart';
+import 'taks_widget.dart' show TaksWidget;
+import 'package:flutter/material.dart';
+
+class TaksModel extends FlutterFlowModel<TaksWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
